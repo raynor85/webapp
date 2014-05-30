97152424e4b7881d327f93397e9d65cfa1808501
fabianpiau <fabian.piau@gmail.com>
2014-05-30 18:08:56 +0200

    Update many frameworks

--- src/main/resources/sql/import.sql
+++ src/main/resources/sql/import.sql
@@ -8,7 +8,11 @@ DELETE FROM userconnection;
 CREATE UNIQUE INDEX userconnectionrank ON userconnection(userid, providerid, rank);
 CREATE UNIQUE INDEX personemail ON person(email);
 
--- Test data
+-- Application references
+INSERT INTO applicationreference(id, creationdate, updatedate, version, active, globalurl, iconinternalurl, name) VALUES (nextval('application_reference_seq'), now(), null, 0, true, 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/latest/', 'firefox.png', 'Firefox');
+INSERT INTO applicationreference(id, creationdate, updatedate, version, active, globalurl, iconinternalurl, name) VALUES (nextval('application_reference_seq'), now(), null, 0, true, 'https://www.apple.com/itunes/download/', 'itunes.png', 'iTunes');
+
+-- Users
 -- test@updapy.com / UpdapyPwd
 INSERT INTO person(id, creationdate, updatedate, version, early, email, name, password, activationdate, active, generationkeydate, key) VALUES (nextval('person_seq'), now(), null, 0, false, 'test@updapy.com', 'Updapy', '$2a$10$faNVhJX.ZrvmpklDMFa.4OBWwG4GbYEQCFzTPaR9hJ.aHYU7zCRrC', now(), true, now(), 'i8v3wb35qz17pxfa0exzmoy18gsmwwwzobhu5ne2nubb1hxs3i');
 INSERT INTO helpmessage(id, creationdate, updatedate, version, hidden, type, person_id) VALUES (nextval('help_message_seq'), now(), null, 0, false, 'DASHBOARD_HOW_TO', currval('person_seq'));
