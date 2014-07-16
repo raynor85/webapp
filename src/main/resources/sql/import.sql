5cb3a6921aa6118c243bde4807920274fd79c2f9
fabianpiau <fabian.piau@gmail.com>
2014-07-16 11:44:39 +0200

    Add KeePass Pro app

--- src/main/resources/sql/import.sql
+++ src/main/resources/sql/import.sql
@@ -84,6 +84,9 @@ INSERT INTO applicationreference(id, creationdate, updatedate, dbversion, active
 INSERT INTO applicationreference(id, creationdate, updatedate, dbversion, active, globalurl, iconfilename, name, apiname, notified, category) VALUES (nextval('application_reference_seq'), now(), null, 0, true, 'http://www.xnview.com/en/xnviewmp/', 'xnviewmp.png', 'XnViewMp', 'xnviewmp', false, 'APPLICATION');
 -- VMware Workstation
 INSERT INTO applicationreference(id, creationdate, updatedate, dbversion, active, globalurl, iconfilename, name, apiname, notified, category) VALUES (nextval('application_reference_seq'), now(), null, 0, true, 'https://my.vmware.com/web/vmware/info/slug/desktop_end_user_computing/vmware_workstation/10_0', 'vmwareworkstation.png', 'VMware Workstation', 'vmwareworkstation', false, 'APPLICATION');
+-- KeePass pro
+INSERT INTO applicationreference(id, creationdate, updatedate, dbversion, active, globalurl, iconfilename, name, apiname, notified, category) VALUES (nextval('application_reference_seq'), now(), null, 0, true, 'http://keepass.info/download.html', 'keepasspro.png', 'KeePass Pro', 'keepasspro', false, 'APPLICATION');
+
 
 -- Application versions
 INSERT INTO applicationversion(id, creationdate, updatedate, dbversion, versiondate, versionnumber, win32urlen, win32urlfr, win64urlen, win64urlfr, application_id) VALUES (nextval('application_version_seq'), now(), NULL, 0, '2014-05-09 00:00:00.643', '29.0.1', 'https://download.mozilla.org/?product=firefox-29.0.1-SSL&os=win&lang=en-US', 'https://download.mozilla.org/?product=firefox-29.0.1-SSL&os=win&lang=fr', NULL, NULL, (select id from applicationreference where apiname = 'firefox'));
