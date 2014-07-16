97cc48d4ced80c21bb52bc9fc3f6a48d18f8c377
fabianpiau <fabian.piau@gmail.com>
2014-07-16 20:24:30 +0200

    Add Skype and Spotify apps

---
+++ src/main/java/com/updapy/service/impl/retriever/SpotifyRemoteRetriever.java
@@ -0,0 +1,44 @@
+package com.updapy.service.impl.retriever;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import com.updapy.model.ApplicationReference;
+import com.updapy.service.retriever.RemoteRetriever;
+import com.updapy.util.ParsingUtils;
+
+@Component
+public class SpotifyRemoteRetriever implements RemoteRetriever {
+
+	@Override
+	public boolean support(ApplicationReference application) {
+		return application.getName().equalsIgnoreCase("Spotify");
+	}
+
+	@Override
+	public String retrieveWin64UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin64UrlEn(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlEn(Document doc) {
+		return StringUtils.removePattern(doc.select("a:contains(Direct Download Link)").attr("href"), "&onid=.*$");
+	}
+
+	@Override
+	public String retrieveVersionNumber(Document doc) {
+		return ParsingUtils.extractVersionNumberFromString(doc.select(".product-landing-quick-specs-row").select(".product-landing-quick-specs-row-content").get(0).text());
+	}
+
+}
