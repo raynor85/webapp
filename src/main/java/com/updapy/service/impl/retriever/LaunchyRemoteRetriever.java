a69db312b85adff2a979bbabbddb31ac14e2c99f
fabianpiau <fabian.piau@gmail.com>
2014-09-17 18:24:29 +0200

    Add Foobar2000, TortoiseSVN, tortoiseGit and Launchy app

---
+++ src/main/java/com/updapy/service/impl/retriever/LaunchyRemoteRetriever.java
@@ -0,0 +1,45 @@
+package com.updapy.service.impl.retriever;
+
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import com.updapy.model.ApplicationReference;
+import com.updapy.service.retriever.RemoteRetriever;
+import com.updapy.util.ParsingUtils;
+
+@Component
+public class LaunchyRemoteRetriever implements RemoteRetriever {
+
+	private static final String ROOT_DOWNLOAD_WEBSITE = "http://www.launchy.net/";
+
+	@Override
+	public boolean support(ApplicationReference application) {
+		return application.getApiName().equalsIgnoreCase("launchy");
+	}
+
+	@Override
+	public String retrieveWin64UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin64UrlEn(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlEn(Document doc) {
+		return ROOT_DOWNLOAD_WEBSITE + doc.select("a[href*=.exe]:contains(Latest Stable Version)").attr("href");
+	}
+
+	@Override
+	public String retrieveVersionNumber(Document doc) {
+		return ParsingUtils.extractVersionNumberFromString(doc.select("a[href*=.exe]:contains(Latest Stable Version)").text());
+	}
+
+}
