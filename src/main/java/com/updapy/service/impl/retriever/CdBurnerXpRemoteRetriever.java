ad5b66bb0340c459697dec6cfdacaff6a9d9c992
fabianpiau <fabian.piau@gmail.com>
2014-09-19 12:52:56 +0200

    Add many apps

---
+++ src/main/java/com/updapy/service/impl/retriever/CdBurnerXpRemoteRetriever.java
@@ -0,0 +1,43 @@
+package com.updapy.service.impl.retriever;
+
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import com.updapy.model.ApplicationReference;
+import com.updapy.service.retriever.RemoteRetriever;
+import com.updapy.util.ParsingUtils;
+
+@Component
+public class CdBurnerXpRemoteRetriever implements RemoteRetriever {
+
+	@Override
+	public boolean support(ApplicationReference application) {
+		return application.getApiName().equalsIgnoreCase("cdburnerxp");
+	}
+
+	@Override
+	public String retrieveWin64UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin64UrlEn(Document doc) {
+		return doc.select("a[href*=x64]:contains(64 bit)").attr("href");
+	}
+
+	@Override
+	public String retrieveWin32UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlEn(Document doc) {
+		return doc.select("a[href*=.exe]:contains(32 bit)").attr("href");
+	}
+
+	@Override
+	public String retrieveVersionNumber(Document doc) {
+		return ParsingUtils.extractVersionNumberFromString(doc.select("h3").text());
+	}
+
+}
---
+++ src/main/java/com/updapy/service/impl/retriever/FeedDemonRemoteRetriever.java
@@ -0,0 +1,43 @@
+package com.updapy.service.impl.retriever;
+
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import com.updapy.model.ApplicationReference;
+import com.updapy.service.retriever.RemoteRetriever;
+import com.updapy.util.ParsingUtils;
+
+@Component
+public class FeedDemonRemoteRetriever implements RemoteRetriever {
+
+	@Override
+	public boolean support(ApplicationReference application) {
+		return application.getApiName().equalsIgnoreCase("feeddemon");
+	}
+
+	@Override
+	public String retrieveWin64UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin64UrlEn(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlEn(Document doc) {
+		return doc.select("a[href*=.exe]:contains(Download FeedDemon)").attr("href");
+	}
+
+	@Override
+	public String retrieveVersionNumber(Document doc) {
+		return ParsingUtils.extractVersionNumberFromString(doc.select("h1").text());
+	}
+
+}
