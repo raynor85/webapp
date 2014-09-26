481597dc231ced5ba25467961a3f726c3903c516
fabianpiau <fabian.piau@gmail.com>
2014-09-26 16:11:36 +0200

    Add Sumatra PDF and WinDjView apps

---
+++ src/main/java/com/updapy/service/impl/retriever/SumatraPdfRemoteRetriever.java
@@ -0,0 +1,51 @@
+package com.updapy.service.impl.retriever;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import com.updapy.model.ApplicationReference;
+import com.updapy.service.retriever.RemoteRetriever;
+import com.updapy.util.ParsingUtils;
+
+@Component
+public class SumatraPdfRemoteRetriever implements RemoteRetriever {
+
+	private static final String PATTERN_VERSION = "{version}";
+	private static final String ROOT_DOWNLOAD_WEBSITE = "https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-" + PATTERN_VERSION + "-install.exe";
+
+	@Override
+	public boolean support(ApplicationReference application) {
+		return application.getApiName().equalsIgnoreCase("sumatrapdf");
+	}
+
+	@Override
+	public String retrieveWin64UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin64UrlEn(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlEn(Document doc) {
+		return StringUtils.replace(ROOT_DOWNLOAD_WEBSITE, PATTERN_VERSION, getVersionNumber(doc));
+	}
+
+	@Override
+	public String retrieveVersionNumber(Document doc) {
+		return getVersionNumber(doc);
+	}
+
+	private String getVersionNumber(Document doc) {
+		return ParsingUtils.extractVersionNumberFromString(StringUtils.removePattern(StringUtils.removePattern(doc.select("body").text(), "gPrevSumatraVersion.*$"), "^.*gSumVer"));
+	}
+
+}
