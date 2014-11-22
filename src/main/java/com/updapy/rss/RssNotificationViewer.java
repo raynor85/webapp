a1c53682f6649c45969036bfebcaf67fa03d7f25
fabianpiau <fabian.piau@gmail.com>
2014-11-22 23:15:22 +0000

    Update framework versions

--- src/main/java/com/updapy/rss/RssNotificationViewer.java
+++ src/main/java/com/updapy/rss/RssNotificationViewer.java
@@ -10,9 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
 
-import com.sun.syndication.feed.rss.Channel;
-import com.sun.syndication.feed.rss.Content;
-import com.sun.syndication.feed.rss.Item;
+import com.rometools.rome.feed.rss.Channel;
+import com.rometools.rome.feed.rss.Content;
+import com.rometools.rome.feed.rss.Item;
 import com.updapy.rss.model.RssNotification;
 import com.updapy.util.MessageUtils;
 
