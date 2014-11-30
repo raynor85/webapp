38843afa66a98b42a06118eafa48cf9e8bb23f12
fabianpiau <fabian.piau@gmail.com>
2014-11-30 01:19:28 +0000

    Fix subsonic parser

--- src/main/java/com/updapy/service/impl/retriever/SubsonicRemoteRetriever.java
+++ src/main/java/com/updapy/service/impl/retriever/SubsonicRemoteRetriever.java
@@ -34,12 +34,12 @@ public class SubsonicRemoteRetriever implements RemoteRetriever {
 
 	@Override
 	public String retrieveWin32UrlEn(Document doc) {
-		return ROOT_DOWNLOAD_WEBSITE + doc.select("a:contains(Download)[href*=.exe]").get(0).attr("href");
+		return ROOT_DOWNLOAD_WEBSITE + doc.select("a[href*=.exe]").get(0).attr("href");
 	}
 
 	@Override
 	public String retrieveVersionNumber(Document doc) {
-		return ParsingUtils.extractVersionNumberFromString(doc.select("th.featurename:contains(stable release)").text());
+		return ParsingUtils.extractVersionNumberFromString(doc.select("a[href*=.exe]").text());
 	}
 
 }
