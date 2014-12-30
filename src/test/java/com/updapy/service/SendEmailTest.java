ebd7d1d7709bbbacf9a30213db0f26be59c7030a
fabianpiau <fabian.piau@gmail.com>
2014-12-30 19:39:59 +0000

    Add message

--- src/test/java/com/updapy/service/SendEmailTest.java
+++ src/test/java/com/updapy/service/SendEmailTest.java
@@ -16,7 +16,7 @@ public class SendEmailTest {
 	EmailSenderService emailSenderService;
 
 	@Test
-	public void sendPersonalEmail() {
+	public void sendPersonalEmailAnswer1En() {
 		String email = "";
 		String subject = "Error when connecting to Updapy via Google +";
 		String title = "Thanks for reporting your problem";
@@ -31,6 +31,16 @@ public class SendEmailTest {
 		String subject = "A propos de vos demandes d'ajout";
 		String title = "Merci pour vos récentes demandes d'ajout d'application";
 		String message = "Malheureusement, nous ne pouvons satisfaire votre demande...<br>Lumia Player et Lumia Browser ne répondent pas à nos exigences pour pouvoir être ajoutés sur Updapy.<br>Nous comptons sur votre compréhension.<br><br>L'équipe Updapy.";
+		Locale locale = Locale.FRENCH;
+		emailSenderService.sendPersonalEmail(email, subject, title, message, locale);
+	}
+	
+	@Test
+	public void sendPersonalEmailAnswer2Fr() {
+		String email = "";
+		String subject = "Réponse à votre message";
+		String title = "Erreur 403";
+		String message = "Merci pour votre retour. Malheureusement, nous ne reproduisons pas votre problème.<br>Une erreur 403 indique un accès refusé.<br>Essayez de vous déconnecter puis de vous reconnecter avant de suivre une nouvelle application.<br>Il est possible qu'un problème de cookie en soit la cause, en particulier si vous utilisez la fonctionnalité \"Se souvenir de moi\".<br><br>L'équipe Updapy.";
 		Locale locale = Locale.FRENCH;
 		emailSenderService.sendPersonalEmail(email, subject, title, message, locale);
 	}
