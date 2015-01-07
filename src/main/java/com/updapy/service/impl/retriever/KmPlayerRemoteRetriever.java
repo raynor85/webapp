8e53d35b51cb481d89c18042073fa5a282dd4e06
fabianpiau <fabian.piau@gmail.com>
2015-01-07 20:23:36 +0000

    Fix KMplayer parser

--- src/main/java/com/updapy/service/impl/retriever/KmPlayerRemoteRetriever.java
+++ src/main/java/com/updapy/service/impl/retriever/KmPlayerRemoteRetriever.java
@@ -1,6 +1,8 @@
 package com.updapy.service.impl.retriever;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import com.updapy.model.ApplicationReference;
@@ -33,7 +35,14 @@ public class KmPlayerRemoteRetriever implements RemoteRetriever {
 
 	@Override
 	public String retrieveWin32UrlEn(Document doc) {
-		return doc.select("div.dm_btn2").select("a").attr("href");
+		Elements buttons = doc.select("div[class^=dm_btn]");
+		Element dlButton;
+		if (buttons.size() > 1) {
+			dlButton = buttons.get(1);
+		} else {
+			dlButton = buttons.get(0);
+		}
+		return dlButton.select("a").attr("href");
 	}
 
 	@Override
