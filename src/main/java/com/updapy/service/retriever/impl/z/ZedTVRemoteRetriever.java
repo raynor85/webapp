2bce21394f5f2c734f9e61580c747a5161f28618
fabianpiau <fabian.piau@gmail.com>
2015-01-25 19:53:37 +0000

    Fix Zed* parsers

--- src/main/java/com/updapy/service/retriever/impl/z/ZedTVRemoteRetriever.java
+++ src/main/java/com/updapy/service/retriever/impl/z/ZedTVRemoteRetriever.java
@@ -39,7 +39,7 @@ public class ZedTVRemoteRetriever implements RemoteRetriever {
 	@Override
 	public String retrieveWin32UrlEn(Document doc) {
 		try {
-			return RemoteServiceImpl.retrieveHtmlDocumentAgent32(DOWNLOAD_WEBSITE).select("a[href*=.exe]").get(0).attr("href").replace("?f=", "f/");
+			return RemoteServiceImpl.retrieveHtmlDocumentAgent32(DOWNLOAD_WEBSITE).select("a[href*=.exe]").get(0).attr("href").replace("?f=", "fich/");
 		} catch (IOException e) {
 			throw new RuntimeException();
 		}
