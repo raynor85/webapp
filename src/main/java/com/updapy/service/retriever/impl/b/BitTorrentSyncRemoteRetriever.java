49c03095b17c0aeff992918a334392eec3a7ce8e
fabianpiau <fabian.piau@gmail.com>
2015-03-04 18:48:27 +0000

    Fix bittorentsync parser

--- src/main/java/com/updapy/service/retriever/impl/b/BitTorrentSyncRemoteRetriever.java
+++ src/main/java/com/updapy/service/retriever/impl/b/BitTorrentSyncRemoteRetriever.java
@@ -1,14 +1,19 @@
 package com.updapy.service.retriever.impl.b;
 
+import java.io.IOException;
+
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 import com.updapy.model.ApplicationReference;
+import com.updapy.service.impl.RemoteServiceImpl;
 import com.updapy.service.retriever.RemoteRetriever;
 import com.updapy.util.ParsingUtils;
 
 @Component
 public class BitTorrentSyncRemoteRetriever implements RemoteRetriever {
+
+	private static final String VERSION_HISTORY_WEBSITE = "http://sync-help.bittorrent.com/customer/portal/articles/1767561-sync-change-log";
 
 	@Override
 	public boolean support(ApplicationReference application) {
@@ -22,7 +27,7 @@ public class BitTorrentSyncRemoteRetriever implements RemoteRetriever {
 
 	@Override
 	public String retrieveWin64UrlEn(Document doc) {
-		return doc.select("a.gat-Downloads-Download_Link-Windows64").get(0).attr("href");
+		return doc.select("button.gat-Downloads-Download_Links-Windows64").get(0).attr("href");
 	}
 
 	@Override
@@ -32,12 +37,16 @@ public class BitTorrentSyncRemoteRetriever implements RemoteRetriever {
 
 	@Override
 	public String retrieveWin32UrlEn(Document doc) {
-		return doc.select("a.gat-Downloads-Download_Link-Windows32").get(0).attr("href");
+		return doc.select("button.gat-Downloads-Download_Links-Windows32").get(0).attr("href");
 	}
 
 	@Override
 	public String retrieveVersionNumber(Document doc) {
-		return ParsingUtils.extractVersionNumberFromString(doc.select("i.fa-windows").get(0).text());
+		try {
+			return ParsingUtils.extractVersionNumberFromString(RemoteServiceImpl.retrieveHtmlDocumentAgent32(VERSION_HISTORY_WEBSITE).select("#support-main").select("strong").first().text());
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
 	}
 
 }
