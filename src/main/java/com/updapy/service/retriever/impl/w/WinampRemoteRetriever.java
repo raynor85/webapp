158c4b0b73cd824fb857947e373b089017639e5f
fabianpiau <fabian.piau@gmail.com>
2015-04-19 10:44:51 +0100

    Add new apps

---
+++ src/main/java/com/updapy/service/retriever/impl/w/WinampRemoteRetriever.java
@@ -0,0 +1,49 @@
+package com.updapy.service.retriever.impl.w;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import com.updapy.model.ApplicationReference;
+import com.updapy.service.retriever.RemoteRetriever;
+import com.updapy.util.ParsingUtils;
+
+@Component
+public class WinampRemoteRetriever implements RemoteRetriever {
+
+	private static final String ROOT_DOWNLOAD_WEBSITE = "http://winampplugins.co.uk/Winamp/";
+
+	@Override
+	public boolean support(ApplicationReference application) {
+		return application.getApiName().equalsIgnoreCase("winamp");
+	}
+
+	@Override
+	public String retrieveWin64UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin64UrlEn(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlFr(Document doc) {
+		return null;
+	}
+
+	@Override
+	public String retrieveWin32UrlEn(Document doc) {
+		return ParsingUtils.buildUrl(ROOT_DOWNLOAD_WEBSITE, getDownloadLink(doc).attr("href"));
+	}
+
+	@Override
+	public String retrieveVersionNumber(Document doc) {
+		return ParsingUtils.extractVersionNumberFromString(getDownloadLink(doc).text());
+	}
+
+	private Elements getDownloadLink(Document doc) {
+		return doc.select("li:contains(Multi-national installer)").select("a");
+	}
+}
