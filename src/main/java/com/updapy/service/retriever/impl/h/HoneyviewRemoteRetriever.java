e9a8f69187a5b7e7adadba0916862373ddc11182
fabianpiau <fabian.piau@gmail.com>
2015-07-13 21:43:12 +0100

    Fix parsers

--- src/main/java/com/updapy/service/retriever/impl/h/HoneyviewRemoteRetriever.java
+++ src/main/java/com/updapy/service/retriever/impl/h/HoneyviewRemoteRetriever.java
@@ -39,7 +39,7 @@ public class HoneyviewRemoteRetriever implements RemoteRetriever {
 
 	@Override
 	public String retrieveWin32UrlEn(Document doc) throws IOException {
-		return HttpUtils.getRedirectionUrl(ParsingUtils.buildUrl(ROOT_DOWNLOAD_WEBSITE, RemoteServiceImpl.retrieveHtmlDocumentAgentMozilla(ParsingUtils.buildUrl(ROOT_DOWNLOAD_WEBSITE, doc.select("a:contains((Bandisoft))").attr("href"))).select("li:contains(from Bandisoft)").select("a").attr("href")));
+		return HttpUtils.getRedirectionUrl(ParsingUtils.buildUrl(ROOT_DOWNLOAD_WEBSITE, RemoteServiceImpl.retrieveHtmlDocumentAgentMozilla(ParsingUtils.buildUrl(ROOT_DOWNLOAD_WEBSITE, doc.select("a:contains(Download Honeyview)").attr("href"))).select("li:contains(from Bandisoft)").select("a").attr("href")));
 	}
 
 	@Override
