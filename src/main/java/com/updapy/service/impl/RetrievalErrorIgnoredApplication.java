144127cc2aac82288abed3652e76074c558e35f9
fabian.piau <fabian.piau@gmail.com>
2017-01-07 17:46:06 +0000

    Updated lists of ignored apps

--- src/main/java/com/updapy/service/impl/RetrievalErrorIgnoredApplication.java
+++ src/main/java/com/updapy/service/impl/RetrievalErrorIgnoredApplication.java
@@ -9,8 +9,8 @@ public class RetrievalErrorIgnoredApplication {
 	public static final List<String> FULLY_IGNORED_APPLICATIONS = Arrays.asList("tortoisesvn", "glasswire", "manycam", "balsamiq", "autohotkey", "resiliosync", "hotspotshield", "admuncher");
 
 	// Hardcoded list of ignored application when the download link is different for the same version
-	public static final List<String> DIFFERENT_URL_IGNORED_APPLICATIONS = Arrays.asList("chromium", "aviraantivirusfree");
-	
+	public static final List<String> DIFFERENT_URL_IGNORED_APPLICATIONS = Arrays.asList("chromium", "aviraantivirusfree", "vlcmediaplayer", "opera");
+
 	// Hardcoded list of ignored application when the new version of the app has been added manually
-	public static final List<String> VERSION_HIGHER_IGNORED_APPLICATIONS = Arrays.asList("phraseexpress", "comodointernetsecurity");
+	public static final List<String> VERSION_HIGHER_IGNORED_APPLICATIONS = Arrays.asList("phraseexpress", "samsungmagician", "comodointernetsecurity");
 }
