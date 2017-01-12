9bf4925cf9b63b6b0d407c25668ed0762a666b7f
fabian.piau <fabian.piau@gmail.com>
2017-01-12 19:01:24 +0000

    Add app as ignored

--- src/main/java/com/updapy/service/impl/RetrievalErrorIgnoredApplication.java
+++ src/main/java/com/updapy/service/impl/RetrievalErrorIgnoredApplication.java
@@ -9,7 +9,7 @@ public class RetrievalErrorIgnoredApplication {
 	public static final List<String> FULLY_IGNORED_APPLICATIONS = Arrays.asList("tortoisesvn", "glasswire", "manycam", "balsamiq", "autohotkey", "resiliosync", "hotspotshield", "admuncher", "argenteutilities", "javare", "adfender");
 
 	// List of completely ignored application when the download link is different for the same version
-	public static final List<String> DIFFERENT_URL_IGNORED_APPLICATIONS = Arrays.asList("chromium", "aviraantivirusfree", "vlcmediaplayer", "opera", "adobereader");
+	public static final List<String> DIFFERENT_URL_IGNORED_APPLICATIONS = Arrays.asList("chromium", "aviraantivirusfree", "vlcmediaplayer", "opera", "adobereader", "cloudsystembooster");
 
 	// List of completely ignored application when the local version of the app is higher than the remote version (e.g it has been added manually)
 	public static final List<String> LOCAL_VERSION_HIGHER_IGNORED_APPLICATIONS = Arrays.asList("phraseexpress", "samsungmagician", "comodointernetsecurity");
